322a051c47b155a6ba3042bb66f4707ca1d685a8
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-07-26 23:13:09 +0530

    bounce.py using while loop

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,10 +2,15 @@
 #
 # Exercise 1.5
 h = 100
-for i in range(10):
-    h = h*(3/5)
-    print((i + 1), round(h, 4))
+# for i in range(10):
+#     h = h*(3/5)
+#     print((i + 1), round(h, 4))
 
 # second way to write the solution
+bounce = 1
+while bounce <= 10:
+    h = h*(3/5)
+    print(bounce, round(h, 4))
+    bounce += 1
 
     
