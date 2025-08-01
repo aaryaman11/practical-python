0f4187ac4ea4a2826238bda087eaa547c4ff46e6
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-01 18:28:03 +0530

    fixed the float value error. I was converting the strig to float before getting th einput from user. As python tried to convert the default string, hence the value error

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,12 +6,19 @@ principal = 500000.0
 rate = 0.05 # annual interest 
 monthly = 2684.11
 total_paid = 0.0
-time = 0 # moths
+time = 0 # months
+extra_payment = float(input("How much extra payment you want to do: "))
+extra_payment_start_month = float(input("extra payment start month: "))
+extra_payment_last_month = float(input("extra payment last month: "))
+
 while principal > 0:
     # while time < 12:
     if time == 0:
         for i in range(12):
-            principal = principal * (1 + rate / 12) - monthly - 1000
+            if extra_payment_start_month != 0 and time == 0:
+                principal = principal * (1 + rate / 12) - monthly - 1000 - extra_payment_start_month
+            else:
+                principal = principal * (1 + rate / 12) - monthly - 1000
             time += 1
             total_paid += monthly + 1000
     else: 
