e2a64362c875949657a642f8a732e7fb10854ee0
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-01 18:13:21 +0530

    modified mortgage.py by calculating the extra  for 12 months and no. of months. This time used a for loop

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,12 +8,15 @@ monthly = 2684.11
 total_paid = 0.0
 time = 0 # moths
 while principal > 0:
-    while time < 12:
-        principal = principal + principal * rate / 12 - monthly - 1000
+    # while time < 12:
+    if time == 0:
+        for i in range(12):
+            principal = principal * (1 + rate / 12) - monthly - 1000
+            time += 1
+            total_paid += monthly + 1000
+    else: 
+        principal = principal + principal * rate / 12 - monthly
         time += 1
-        total_paid += monthly + 1000 
-    principal = principal + principal * rate / 12 - monthly
-    time += 1
-    total_paid  += monthly
+        total_paid  += monthly
 print('Total_paid', total_paid, 'over', time, "months")
 
