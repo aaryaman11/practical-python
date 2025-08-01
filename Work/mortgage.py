7a6b6a271e5ab289586698b7a6391f7e1539527f
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-01 17:55:28 +0530

    modified mortgage.py by calaculating months now

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,8 +6,10 @@ principal = 500000.0
 rate = 0.05 # annual interest 
 monthly = 2684.11
 total_paid = 0.0
+time = 0 # moths
 while principal > 0:
     principal = principal + principal * rate / 12 - monthly
+    time += 1
     total_paid  += monthly
-print('Toatl_paid', total_paid)
+print('Total_paid', total_paid, 'over', time, "months")
 
