c7c8fc136e304af917a7b215aa50c69deeba49f4
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-01 17:47:43 +0530

    modified mortgage.py by calculating payments on loan

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,13 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05 # annual interest 
+monthly = 2684.11
+total_paid = 0.0
+while principal > 0:
+    principal = principal + principal * rate / 12 - monthly
+    total_paid  += monthly
+print('Toatl_paid', total_paid)
+
