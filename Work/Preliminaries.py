58d59b5f9c6670f43e75fa3b1e0028a832ce9ed3
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-10 14:51:41 +0530

    modified pcost.py to calculate the total cost of shares

--- Work/Preliminaries.py
+++ Work/Preliminaries.py
@@ -1,5 +1,5 @@
 with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',') # advaces the file iterator to the next line, effectively skipping the header
+    headers = next(f).split(',') # advances the file iterator to the next line, effectively skipping the header
 # print(headers) 
     for line in f: # loop starts reading from the second line onward,
         row = line.split(',')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+j = 0
 with open('Data/portfolio.csv', 'rt')  as f:
-    data= f.read()
-print(data)
+    header = next(f).split(',') 
+    for data in f:
+        row = data.strip().split(",")
+        share = int(row[1])
+        prices = float(row[2])
+        j += share*prices
+    print('Total cost', j)
