8e1c15a6282e12f61699c8ffc60f9d3fd006f46a
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-10 13:53:51 +0530

    modified Preliminaries.py to skip header line with next()

--- Work/Preliminaries.py
+++ Work/Preliminaries.py
@@ -1,3 +1,5 @@
 with open('Data/portfolio.csv', 'rt') as f:
-    for line in f:
+    headers = next(f) # advaces the file iterator to the next line, effectively skipping the header
+# print(headers) 
+    for line in f: # loop starts reading from the second line onward,
         print(line, end='')
