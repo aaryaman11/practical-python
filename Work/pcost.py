76657863e64b199fd26569d78eaf5f47940fb1b8
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-08-27 18:18:17 +0530

    Created Other_Kind_File to understad how to use gzip to open compressed zip files, normal open method wouldn't work

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
-j = 0
-with open('Data/portfolio.csv', 'rt')  as f:
-    header = next(f).split(',') 
-    for data in f:
-        row = data.strip().split(",")
-        share = int(row[1])
-        prices = float(row[2])
-        j += share*prices
-    print('Total cost', j)
+
+# with open('Data/portfolio.csv', 'rt')  as f:
+#     header = next(f).split(',') 
+#     for data in f:
+#         row = data.strip().split(",")
+#         share = int(row[1])
+#         prices = float(row[2])
+#         j += share*prices
+#     print('Total cost', j)
+
+def portfoloio_cost(filename):
+    j = 0
+    with open(filename, 'rt')  as f:
+        header = next(f).split(',') 
+        for data in f:
+            row = data.strip().split(",")
+            share = int(row[1])
+            prices = float(row[2])
+            j += share*prices
+    return j
+
+cost = portfoloio_cost('Data/portfolio.csv')
+print('Total cost: ', cost)
