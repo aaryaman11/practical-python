f986a5674fd1e77567de430f6ecb84117b1dc9c0
Aaryaman Sharma <aaryaman.sh@gmail.com>
2025-09-04 16:15:57 +0530

    Modified pcost.py with import module csv for formatting

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,8 @@
 #
 # Exercise 1.27
 
+import csv
+
 # with open('Data/portfolio.csv', 'rt')  as f:
 #     header = next(f).split(',') 
 #     for data in f:
@@ -14,14 +16,18 @@
 def portfoloio_cost(filename):
     j = 0
     with open(filename, 'rt')  as f:
-        header = next(f).split(',') 
-        for data in f:
-            row = data.strip().split(",")
+        row = csv.reader(f)
+        # print(row)
+        header = next(row)
+        # print('Header: ', header)
+        for data in row:
+            # print('Data: ', data[1])
+            
             try:
-                share = int(row[1])
-                prices = float(row[2])
+                share = int(data[1])
+                prices = float(data[2])
             except ValueError:
-                print('Missing data', row)
+                print('Missing data', data)
                 continue
             # share = int(row[1])
             # prices = float(row[2])
