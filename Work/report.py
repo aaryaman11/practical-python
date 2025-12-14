838460e7575ae5109fc1d112caa3a4c02ab5eded
aaryaman11 <aaryaman.sh@gmail.com>
2025-12-15 01:39:04 +0530

    2.7 completion

--- Work/report.py
+++ Work/report.py
@@ -5,6 +5,7 @@ import csv
 from pprint import pprint
 def read_portfolio(filename):
     portfolio =  []
+    # total_value = 0.0
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         header = next(rows)
@@ -15,9 +16,34 @@ def read_portfolio(filename):
                 'price': float(row[2])
                 }
             portfolio.append(holdings)
+            # total_value += holdings['share'] * holdings['price']
+
+    # print('Total value of portfolio: ', total_value)
     return portfolio
 
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if not row:
+               continue
+            prices[row[0]] = float(row[1])
+    return prices
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
-pprint(portfolio)
+prices = read_prices('Data/prices.csv')
+def compute_value(portfolio, prices):
+    total_value = 0.0
+    gain_loss = 0.0
+    for holding in portfolio:
+        total_value += holding['price'] * holding['share']
+        gain_loss += (prices[holding['name']] - holding['price']) * holding['share']
+    return total_value, gain_loss
+
+total_value, gain_loss = compute_value(portfolio, prices)
+print('Total value of portfolio: ', total_value, ' Gain/Loss: ', gain_loss)
+#pprint(portfolio)
 
 
