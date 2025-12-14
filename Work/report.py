ed2320d4598d0cd9ea9bfdc5cb3ecddb9cc9ec1a
aaryaman11 <aaryaman.sh@gmail.com>
2025-12-14 22:28:59 +0530

    report.py complete

--- Work/report.py
+++ Work/report.py
@@ -2,17 +2,22 @@
 #
 # Exercise 2.4
 import csv
+from pprint import pprint
 def read_portfolio(filename):
     portfolio =  []
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         header = next(rows)
         for row in rows:
-            holdings = (row[0], int(row[1]), float(row[2]))
+            holdings = {
+                'name': row[0], 
+                'share': int(row[1]), 
+                'price': float(row[2])
+                }
             portfolio.append(holdings)
     return portfolio
 
 portfolio = read_portfolio('Data/portfolio.csv')
-print(portfolio)
+pprint(portfolio)
 
 
