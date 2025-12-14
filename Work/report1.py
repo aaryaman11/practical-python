d71e15380049dbf353f2165766301a4988198f59
aaryaman11 <aaryaman.sh@gmail.com>
2025-12-15 00:05:39 +0530

    report1.py with dictionary changes

--- Work/report1.py
+++ Work/report1.py
@@ -1,10 +1,17 @@
 import csv
+from pprint import pprint
 
 def read_prices(filename):
     prices = {}
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
-            prices[row[0]] = row[1]
-        print(prices)
-print(read_prices('Data/prices.csv'))
+            # try:
+            #     prices[row[0]] = float(row[1])
+            # except IndexError:
+            #     continue
+            if not row:
+               continue
+            prices[row[0]] = float(row[1])
+    return prices
+pprint(read_prices('Data/prices.csv'))
